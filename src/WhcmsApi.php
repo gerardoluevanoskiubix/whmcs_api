1bb8b7a494492bdb490453834b43ec5e6658db20
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2023-07-07 17:22:08 -0600

    validador de configuracion

--- src/WhcmsApi.php
+++ src/WhcmsApi.php
@@ -4,24 +4,48 @@ namespace WhcmsApi;
 
 class WhcmsApi{
 
-    public function boot()
-    {
-        // Publicar archivos de configuración
-        $this->publishConfig();
+    public function validateconfigbeforecontinue(array $config){
+        try{
+            if(is_array($config)){
+                $keysToCheck = ['url', 'identifier', 'secret', 'url_path'];
+                $containsAllKeys = true;
+                foreach ($keysToCheck as $key) {
+                    if (!array_key_exists($key, $arr['sandbox'])) {
+                        $containsAllKeys = false;
+                        break;
+                    }
+                }
+                if ($containsAllKeys) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }else{
+                return false;
+            } 
+        }catch(Exception $e){
+            return $e;
+        }
     }
 
-    public function getclient(){
-        return $this->getclientAsync();
+    public function getclient(array $config,$response_in_array = false,array $client){
+       $continue = $this->validateconfigbeforecontinue($config);
+       if($continue && is_array($client)){
+            return $this->getclientAsync($config,$client);
+       }else{
+        if($response_in_array){
+            $res = array(
+                "status" => false,
+                'message' => "The config client is not valid"
+            );
+            return $res;
+        }else{
+            return false;
+        }
+       }
     }
 
-    public function getclientAsync(){
-        return 'holi';
-    }
-
-    protected function publishConfig()
-    {
-        $configSourcePath = __DIR__ . '/../../config/config.php';
-        $configDestinationPath = config_path('whcms.php');
-        copy($configSourcePath, $configDestinationPath);
+    public function getclientAsync($config,$user){
+        return $user;
     }
 }
