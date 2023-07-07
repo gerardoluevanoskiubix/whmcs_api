dfd0c5191ee46451b8ccdeca11f2f9450b906236
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2023-07-07 16:25:11 -0600

    config file

--- src/WhcmsApi.php
+++ src/WhcmsApi.php
@@ -3,11 +3,25 @@
 namespace WhcmsApi;
 
 class WhcmsApi{
+
+    public function boot()
+    {
+        // Publicar archivos de configuración
+        $this->publishConfig();
+    }
+
     public function getclient(){
         return $this->getclientAsync();
     }
 
     public function getclientAsync(){
         return 'holi';
+    }
+
+    protected function publishConfig()
+    {
+        $configSourcePath = __DIR__ . '/../../config/config.php';
+        $configDestinationPath = config_path('whcms.php');
+        copy($configSourcePath, $configDestinationPath);
     }
 }
