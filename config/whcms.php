1165a31bdf06fe3d1a3dd0fee58907add0d70c86
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2023-07-07 16:11:57 -0600

    config file

--- config/whcms.php
+++ config/whcms.php
@@ -1,4 +1,21 @@
 <?php
-return[
-    'whcms_url' => env('WHCMS_URL', 'localhost:8000')
+/**
+ * WHCMS Setting & API Credentials
+ * Created by Gerardo Luevanos <gerardo.luevanos01@gmail.com>.
+ */
+return [
+    'mode'    => env('WHCMS_MODE', 'sandbox'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+    'sandbox' => [
+        'url'                 => env('WHCMS_SANDBOX_URL',''),
+        'identifier'          => env('WHCMS_SANDBOX_IDENTIFIER', ''),
+        'secret'              => env('WHCMS_SANDBOX_SECRET', ''),
+        'url_path'            => env('WHCMS_SANDBOX_URL_PATH',''),
+    ],
+    'live' => [
+        'url'                 => env('WHCMS_LIVE_URL',''),
+        'identifier'          => env('WHCMS_LIVE_IDENTIFIER', ''),
+        'secret'              => env('WHCMS_LIVE_SECRET', ''),
+        'product_one_adminit' => env('WHCMS_LIVE_PRODUCT_ONE_ADMINIT', ''),
+        'url_path'           => env('WHCMS_LIVE_URL_PATH',''),
+    ],
 ];
