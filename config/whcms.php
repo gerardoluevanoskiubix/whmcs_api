f8be678e480e7550cbecd4bbd667d9f331ba27a5
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2023-07-07 15:59:00 -0600

    archivo config

---
+++ config/whcms.php
@@ -0,0 +1,4 @@
+<?php
+return[
+    'whcms_url' => env('WHCMS_URL', 'localhost:8000')
+]
--- src/WhcmsApi.php
+++ src/WhcmsApi.php
@@ -3,11 +3,11 @@
 namespace WhcmsApi;
 
 class WhcmsApi{
-    public static function getclient(){
+    public function getclient(){
         return $this->getclientAsync();
     }
 
-    public static function getclientAsync(){
+    public function getclientAsync(){
         return 'holi';
     }
 }
