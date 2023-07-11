82b3987eacdaf37d05665425b69c0f83602d076a
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2023-07-11 13:25:44 -0600

    readem md

---
+++ Readme.md
@@ -0,0 +1,74 @@
+# Laravel WHMCS Rest API
+    
+<a name="introduction"></a>
+## Documentation / Usage
+
+```php
+// Import the class namespaces first, before using it directly
+use whcms_api\whcms\src\WhcmsApi;
+
+$whcms = new \WhcmsApi\WhcmsApi();
+
+```
+
+<a name="usage-paypal-api-configuration"></a>
+## Configuration File
+
+The configuration file **whcms.php** is located in the **config** folder. Following are its contents when published:
+
+```php
+<?php
+/**
+ * WHCMS Setting & API Credentials
+ * Created by Gerardo Luevanos <gerardo.luevanos01@gmail.com>.
+ */
+return [
+    'mode'    => env('WHCMS_MODE', 'sandbox'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+    'sandbox' => [
+        'url'                 => env('WHCMS_SANDBOX_URL',''),
+        'identifier'          => env('WHCMS_SANDBOX_IDENTIFIER', ''),
+        'secret'              => env('WHCMS_SANDBOX_SECRET', ''),
+        'url_path'            => env('WHCMS_SANDBOX_URL_PATH',''),
+        'type_payment'        => env('WHCMS_SANDBOX_TYPE_PAYMENT',''),
+    ],
+    'live' => [
+        'url'                 => env('WHCMS_LIVE_URL',''),
+        'identifier'          => env('WHCMS_LIVE_IDENTIFIER', ''),
+        'secret'              => env('WHCMS_LIVE_SECRET', ''),
+        'product_one_adminit' => env('WHCMS_LIVE_PRODUCT_ONE_ADMINIT', ''),
+        'url_path'           => env('WHCMS_LIVE_URL_PATH',''),
+        'type_payment'        => env('WHCMS_LIVE_TYPE_PAYMENT',''),
+    ],
+];
+```
+
+ ```php
+    //This is an example to valdate login
+
+    //Initialice method
+    $whcms = new \WhcmsApi\WhcmsApi();
+    //Obtain config status, sandbox or live
+    $mode = config('whcms.mode');
+    //Obtain config in this mode
+    $config = config("whcms.{$mode}");
+    //Create array with information of user to validate login
+    $user = array(
+        "email" => 'gerardo.luevanos@kiubix.com',
+        "password" => 'toker99100.'
+    );
+    //Send data to callback
+    //Send config,response_in_json is default false, change true for false to return bool val, include user config
+    $user = $whcms->validatelogin($config,true,$user);
+    /*** 
+     * if response in json is true return:
+     * "ststus" => bool - true or false,
+     * "message" => "status of callback",
+     * 
+     * if response in json is false return:
+     * boolval true or false
+     * 
+ ```
+
+<a name="support"></a>
+## Support
+*gerardo.luevanos01@gmail.com
--- src/WhcmsApi.php
+++ src/WhcmsApi.php
@@ -28,15 +28,15 @@ class WhcmsApi{
         }
     }
 
-    public function getclient(array $config,$response_in_array = false,array $client){
+    public function validatelogin(array $config,$response_in_array = false,array $user){
        $continue = $this->validateconfigbeforecontinue($config);
-       if($continue && is_array($client)){
-            return $this->getclientAsync($config,$client);
+       if($continue && is_array($user)){
+            return $this->validateloginAsync($config,$response_in_array,$user);
        }else{
         if($response_in_array){
             $res = array(
                 "status" => false,
-                'message' => "The config client is not valid"
+                'message' => "The config user is not valid"
             );
             return $res;
         }else{
@@ -45,7 +45,54 @@ class WhcmsApi{
        }
     }
 
-    public function getclientAsync($config,$user){
-        return $user;
+    public function validateloginAsync($config,$response_in_array,$user){
+        try{
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $config['url']);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,
+                http_build_query(
+                    array(
+                        'action' => 'ValidateLogin',
+                        'username' => $config['identifier'],
+                        'password' => $config['secret'],
+                        'email' => $user['email'],
+                        'password2' => $user['password'],
+                        'responsetype' => 'json',
+                    )
+                )
+            );
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $response_in_json = json_decode($response);
+            if($response_in_json->result == 'success'){
+                if($response_in_array){
+                    $res = $response_in_json;
+                    return $res;
+                }else{
+                    return true;
+                }
+            }elseif($response_in_json->result == 'error'){
+                if($response_in_array){
+                    $res = $response_in_json;
+                    return $res;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }catch(Exception $e){
+            if($response_in_array){
+                $res = array(
+                    "status" => false,
+                    'message' => "The config client is not valid"
+                );
+                return $res;
+            }else{
+                return false;
+            }
+        }
     }
 }
