b973fdede127933fc0b2cef17801c3869e8de1a8
Gerardo Luevanos <gerardo.luevanos@kiubix.com>
2023-07-11 15:01:42 -0600

    all functions

--- src/WhcmsApi.php
+++ src/WhcmsApi.php
@@ -73,7 +73,71 @@ class WhcmsApi{
     }
 
     public function addorder(array $config,$response_in_array = true,array $order){
+        $continue = $this->validateconfigbeforecontinue($config);
+        if($continue && is_array($order)){
+             return $this->addorderAsync($config,$response_in_array,$order);
+        }else{
+         if($response_in_array){
+             $res = array(
+                 "status" => false,
+                 'message' => "The config order is not valid"
+             );
+             return $res;
+         }else{
+             return false;
+         }
+        } 
+    }
 
+    public function getinvoice(array $config,$response_in_array = true,$invoice){
+        $continue = $this->validateconfigbeforecontinue($config);
+        if($continue && $invoice){
+             return $this->getinvoiceAsync($config,$response_in_array,$order);
+        }else{
+         if($response_in_array){
+             $res = array(
+                 "status" => false,
+                 'message' => "The config order is not valid"
+             );
+             return $res;
+         }else{
+             return false;
+         }
+        } 
+    }
+
+    public function addinvoicepayment(array $config,$response_in_array = true,array $invoice){
+        $continue = $this->validateconfigbeforecontinue($config);
+        if($continue && is_array($order)){
+             return $this->addinvoicepaymentAsync($config,$response_in_array,$order);
+        }else{
+         if($response_in_array){
+             $res = array(
+                 "status" => false,
+                 'message' => "The config order is not valid"
+             );
+             return $res;
+         }else{
+             return false;
+         }
+        } 
+    }
+    
+    public function createquote(array $config,$response_in_array = true,array $quote){
+        $continue = $this->validateconfigbeforecontinue($config);
+        if($continue && is_array($quote)){
+             return $this->createquoteAsync($config,$response_in_array,$quote);
+        }else{
+         if($response_in_array){
+             $res = array(
+                 "status" => false,
+                 'message' => "The config quote is not valid"
+             );
+             return $res;
+         }else{
+             return false;
+         }
+        } 
     }
 
     public function validateloginAsync($config,$response_in_array,$user){
@@ -112,7 +176,7 @@ class WhcmsApi{
                         'groupid' => $config['brand_id'],
                     )
                 );
-        return $this->sendrequestandmethod($config,$query);     
+        return $this->sendrequestandmethod($config,$response_in_array,$query);     
     }
 
     public function getclientAsync($config,$response_in_array,$email){
@@ -139,7 +203,76 @@ class WhcmsApi{
                         'responsetype' => 'json',
                     )
                 );
-        return $this->sendrequestandmethod($config,$query);
+        return $this->sendrequestandmethod($config,$response_in_array,$query);
+    }
+
+    public function addorderAsync($config,$response_in_array,$order){
+        $query = http_build_query(
+                    array(
+                        'action' => 'AddOrder',
+                        'username' => $config['identifier'],
+                        'password' => $config['secret'],
+                        'clientid' => $order['clientid'],
+                        'pid' => $order['pid'],
+                        'addons' => $order['addons'],
+                        'domain' => $order['domain'],
+                        'billingcycle' => $order['billingcycle'],
+                        'paymentmethod' => $order['paymentmethod'],
+                        'responsetype' => 'json',
+                        'noemail' => $order['noemail'],
+                        'noinvoiceemail' => $order['noinvoiceemail'],
+                        'brandid' => $config['brand_id'],
+                    )
+                );
+        return $this->sendrequestandmethod($config,$response_in_array,$query);
+    }
+
+    public function getinvoiceAsync($config,$response_in_array,$invoice){
+        $query = http_build_query(
+                    array(
+                        'action' => 'GetInvoice',
+                        'username' => $config['identifier'],
+                        'password' => $config['secret'],
+                        'invoiceid' => $invoice,
+                        'responsetype' => 'json',
+                    )
+                );
+        return $this->sendrequestandmethod($config,$response_in_array,$query);
+    }
+
+    public function addinvoicepaymentAsync($config,$response_in_array,$invoice){
+        $query = http_build_query(
+                    array(
+                        'action' => 'AddInvoicePayment',
+                        'username' => $config['identifier'],
+                        'password' => $config['secret'],
+                        'invoiceid' => $invoice['id'],
+                        'transid' => $invoice['transid'],
+                        'gateway' => $invoice['gateway'],
+                        'date' => $invoice['date'],
+                        'noemail' => $invoice['noemail'],
+                        'responsetype' => 'json',
+                    )
+                );
+        return $this->sendrequestandmethod($config,$response_in_array,$query);
+    }
+
+    public function createquoteAsync($config,$response_in_array,$quote){
+        $query = ttp_build_query(
+                    array(
+                        'action' => 'CreateQuote',
+                        'username' => $config['identifier'],
+                        'password' => $config['secret'],
+                        'subject' => $quote['subject'],
+                        'stage' => 'Draft',
+                        'userid' => $quote['user_id'],
+                        'datecreated' => $quote['date_created'],
+                        'validuntil' => $quote['dateuntil'],
+                        'lineitems' => base64_encode(serialize($quote['array_items_quote'])),
+                        'responsetype' => 'json',
+                    )
+                );
+        return $this->sendrequestandmethod($config,$response_in_array,$query);
     }
 
     public function sendrequestandmethod($config,$response_in_array,$query){
@@ -181,7 +314,7 @@ class WhcmsApi{
             }
         }
     }
-
+    
     public function validateconfigbeforecontinue(array $config){
         try{
             if(is_array($config)){
